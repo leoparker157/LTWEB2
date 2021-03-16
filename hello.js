e8b5cc9b72d6f05289b638fcf533539c95b7bba5
Kien <Leoparker157@gmail.com>
2021-03-16 21:17:48 +0700

    update

---
+++ hello.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+    res.send("hello world");
+});
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port} !`);
+});
